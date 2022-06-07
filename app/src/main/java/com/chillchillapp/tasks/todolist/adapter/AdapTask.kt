349576d8de996e99583cdbb2c852733a3b624aad
63OO36O PHASIT YAPRASAT <phasit.y63@rsu.ac.th>
2022-06-07 20:26:34 +0700

    -edit design task list

--- app/src/main/java/com/chillchillapp/tasks/todolist/adapter/AdapTask.kt
+++ app/src/main/java/com/chillchillapp/tasks/todolist/adapter/AdapTask.kt
@@ -5,23 +5,25 @@ import android.content.Context
 import android.content.Intent
 import android.text.SpannableString
 import android.text.style.StrikethroughSpan
+import android.util.Log
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.annotation.AttrRes
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.chillchillapp.tasks.todolist.*
 import com.chillchillapp.tasks.todolist.database.*
 import com.chillchillapp.tasks.todolist.master.RepeatHelper
 import com.chillchillapp.tasks.todolist.model.ModelTask
+import com.chillchillapp.tasks.todolist.model.ModelTaskSub
+import kotlinx.android.synthetic.main.activity_initiate_language.*
+import kotlinx.android.synthetic.main.activity_language.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -67,6 +69,12 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
         setFavorite(holder, position)
         setState(holder, position)
         setEvent(holder, position)
+
+//        val subtaskList = functionSubTask.getDataByTaskId(taskList[position].id)
+//        val adapter = AdapSubTask(subtaskList)
+//        val layoutManager = GridLayoutManager(activity,  1, GridLayoutManager.VERTICAL, false)
+//        holder.subtaskRCV.adapter = adapter
+//        holder.subtaskRCV.layoutManager = layoutManager
     }
 
     private fun setName(holder: ViewHolder, position: Int){
@@ -218,22 +226,16 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
     private fun setEvent(holder: ViewHolder, position: Int){
 
         holder.itemFavoriteRL.setOnClickListener {
-
-            taskList[position].favorite = if(taskList[position].favorite == 0L){
-                1
-            }else{
-                0
-            }
+            taskList[position].favorite = if(taskList[position].favorite == 0L) 1 else 0
 
             setFavorite(holder, position)
 
             if(updateTask(taskList[position]) != 0){
-                Toast.makeText(activity, "เพิ่มในรายการโปรดเรียบร้อย", Toast.LENGTH_SHORT).show()
+                Toast.makeText(activity, activity.getString(R.string.Successfully_added_to_favorites), Toast.LENGTH_SHORT).show()
             }
         }
 
         holder.itemStateRL.setOnClickListener {
-
             when(taskList[position].state){
                 0L->{
                     taskList[position].state = 1
@@ -242,7 +244,6 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
                     taskList[position].state = 0
                 }
             }
-
             setState(holder, position)
 
             // is repeat
@@ -270,9 +271,24 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
         holder.itemCV.setOnClickListener{
             val intent = Intent(activity, InputTasksActivity::class.java)
             intent.putExtra(COL_TASK_ID, taskList[position].id)
-            intent.putExtra("function", "Update")
+            intent.putExtra(KEY_FUNCTION, KEY_UPDATE)
             activity.startActivity(intent)
+
+            //expandTask(holder)
         }
+    }
+
+    private fun expandTask(holder: ViewHolder){
+        Log.i(TAG, "expandTask")
+
+        if(holder.subtaskLL.visibility == View.VISIBLE){
+            holder.subtaskLL.visibility = View.GONE
+            holder.itemCV.cardElevation = 0f
+        }else{
+            holder.subtaskLL.visibility = View.VISIBLE
+            holder.itemCV.cardElevation = 8f
+        }
+
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -291,6 +307,9 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
 
         val itemFavoriteRL = itemView.findViewById<RelativeLayout>(R.id.itemFavoriteRL)
         val itemStateRL = itemView.findViewById<RelativeLayout>(R.id.itemStateRL)
+
+        val subtaskLL = itemView.findViewById<LinearLayout>(R.id.subtaskLL)
+        val subtaskRCV = itemView.findViewById<RecyclerView>(R.id.subtaskRCV)
 
     }
 
@@ -321,7 +340,7 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
         when(state){
             "done"->{
 
-                //holder.stateIV.setColorFilter(activity.getColorTheme(R.attr.colorAccent))
+                holder.stateIV.setColorFilter(themeColor(activity, R.attr.colorAccent))
                 holder.stateIV.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_ok_filled))
                 holder.nameTV.setTextColor(ContextCompat.getColor(activity, R.color.colorWhiteDarkDark))
 
@@ -349,6 +368,77 @@ class AdapTask(private val activity: Activity, private var taskList: ArrayList<M
 
     override fun getItemCount(): Int {
         return taskList.size
+    }
+
+
+    //subtask of Task
+    inner class AdapSubTask(private var subTakList: ArrayList<ModelTaskSub>): RecyclerView.Adapter<AdapSubTask.ViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapSubTask.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.listview_task_subtask, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: AdapSubTask.ViewHolder, position: Int) {
+
+            when(subTakList[position].state){
+                1L->{
+                    holder.stateIV.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_ok_filled))
+                    holder.nameTV.setTextColor(ContextCompat.getColor(activity, R.color.colorWhiteDarkDark))
+                    val ss = strikethroughSpan(subTakList[position].todo!!)
+                    holder.nameTV.text = ss
+
+                }
+                0L->{
+                    holder.stateIV.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_round))
+                    holder.stateIV.setColorFilter(ContextCompat.getColor(activity, R.color.colorWhiteDarkDark))
+                    holder.nameTV.setTextColor(ContextCompat.getColor(activity, R.color.colorBlack))
+
+                    holder.nameTV.text = subTakList[position].todo
+
+                }
+            }
+
+            setEvent(holder, position)
+        }
+
+        private fun setEvent(holder: ViewHolder, position: Int){
+
+            holder.stateIV.setOnClickListener {
+                when(subTakList[position].state){
+                    1L->{
+                        subTakList[position].state = 0L
+                        subTakList[position].updateDate = System.currentTimeMillis()
+                        functionSubTask.update(subTakList[position])
+
+                        holder.stateIV.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_round))
+                        holder.stateIV.setColorFilter(ContextCompat.getColor(activity, R.color.colorWhiteDarkDark))
+                        holder.nameTV.setTextColor(ContextCompat.getColor(activity, R.color.colorBlack))
+
+                        holder.nameTV.text = subTakList[position].todo
+
+                    }
+                    0L->{
+                        subTakList[position].state = 1L
+                        subTakList[position].updateDate = System.currentTimeMillis()
+                        functionSubTask.update(subTakList[position])
+
+                        holder.stateIV.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_ok_filled))
+                        holder.nameTV.setTextColor(ContextCompat.getColor(activity, R.color.colorWhiteDarkDark))
+                        val ss = strikethroughSpan(subTakList[position].todo!!)
+                        holder.nameTV.text = ss
+
+                    }
+                }
+            }
+        }
+
+        override fun getItemCount(): Int = subTakList.size
+
+        inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val nameTV = itemView.findViewById<TextView>(R.id.nameTV)
+            val stateIV = itemView.findViewById<ImageView>(R.id.stateIV)
+
+        }
     }
 
 }
